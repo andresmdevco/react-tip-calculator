4d955d954fff06689cbea9ec184a9a372097e29d
Andrés Muñoz <andres2078@gmail.com>
2026-06-30 20:46:48 -0500

    feat: add main application layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-
+import { menuItems } from "./data/db"
 
 function App() {
 
-    
+  console.log(menuItems)
+  
   return (
     <>
-      <header className="bg-teal-400 py-5">
+      <header className=" bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
+
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+        
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+
+        </div>
+      </main>
     </>
   )
 }
