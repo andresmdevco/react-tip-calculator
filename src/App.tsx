4a85f14380ff4e8443a68fc96bce2b2981fec80d
Andrés Muñoz <andres2078@gmail.com>
2026-07-27 23:18:24 -0500

    style: format code with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -6,49 +6,35 @@ import { menuItems } from './data/db';
 import useOrder from './hooks/useOrder';
 
 function App() {
-  
-  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder()
+  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder();
 
   return (
     <>
       <header className=" bg-teal-400 py-5">
-        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+        <h1 className="text-center text-4xl font-black">
+          Calculadora de Propinas y Consumo
+        </h1>
       </header>
 
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div className='p-5'>
-          <h2 className='text-4xl font-black'>Menú</h2>
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
 
-          <div className='space-y-3 mt-10'>
-            {menuItems.map(item => (
-              <MenuItem
-                key={item.id}
-                item={item}
-                addItem={addItem}
-              />
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} addItem={addItem} />
             ))}
           </div>
-
         </div>
 
-        <div className='border border-dashed border-slate-300 p-5 rounded-lg space-y-10'>
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
           {order.length ? (
             <>
-              <OrderContents
-                order={order}
-                removeItem={removeItem}
-              />
-    
-              <TipPercentageForm
-                setTip={setTip}
-                tip={tip}
-              />
-    
-              <OrderTotals
-                order={order}
-                tip={tip}
-                placeOrder={placeOrder}
-              />
+              <OrderContents order={order} removeItem={removeItem} />
+
+              <TipPercentageForm setTip={setTip} tip={tip} />
+
+              <OrderTotals order={order} tip={tip} placeOrder={placeOrder} />
             </>
           ) : (
             <p className="text-center">La orden esta vacía</p>
@@ -56,7 +42,7 @@ function App() {
         </div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,32 +1,44 @@
-import { useMemo } from "react"
-import { formatCurrency } from "../helpers"
-import type { OrderItem } from "../types"
+import { useMemo } from 'react';
+import { formatCurrency } from '../helpers';
+import type { OrderItem } from '../types';
 
 type OrderTotalsProps = {
-  order: OrderItem[],
-  tip: number,
-  placeOrder: () => void
-}
+  order: OrderItem[];
+  tip: number;
+  placeOrder: () => void;
+};
 
-export default function OrderTotals({ order, tip, placeOrder } : OrderTotalsProps) {
-
-  const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
-  const tipAmount = useMemo(() => subtotalAmount * tip, [subtotalAmount, tip])
-  const totalAmount = useMemo(() => subtotalAmount + tipAmount, [subtotalAmount, tipAmount])
+export default function OrderTotals({
+  order,
+  tip,
+  placeOrder,
+}: OrderTotalsProps) {
+  const subtotalAmount = useMemo(
+    () => order.reduce((total, item) => total + item.quantity * item.price, 0),
+    [order],
+  );
+  const tipAmount = useMemo(() => subtotalAmount * tip, [subtotalAmount, tip]);
+  const totalAmount = useMemo(
+    () => subtotalAmount + tipAmount,
+    [subtotalAmount, tipAmount],
+  );
 
   return (
     <>
       <div className="space-y-3">
         <h2 className="font-black text-2xl">Totales y Propina:</h2>
-        <p>Subtotal a pagar: {''}
+        <p>
+          Subtotal a pagar: {''}
           <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
         </p>
 
-        <p>Propina: {''}
+        <p>
+          Propina: {''}
           <span className="font-bold">{formatCurrency(tipAmount)}</span>
         </p>
 
-        <p>Total a Pagar: {''}
+        <p>
+          Total a Pagar: {''}
           <span className="font-bold">{formatCurrency(totalAmount)}</span>
         </p>
       </div>
@@ -39,5 +51,5 @@ export default function OrderTotals({ order, tip, placeOrder } : OrderTotalsProp
         Guardar Orden
       </button>
     </>
-  ) 
+  );
 }
